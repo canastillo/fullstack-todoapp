7548815e9256480d9cc7f4b939012b16ec50ac71
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2021-12-02 15:16:00 -0600

    Added SingleTodo logic and styles

--- frontend/src/views/SingleTodo.jsx
+++ frontend/src/views/SingleTodo.jsx
@@ -1,35 +1,66 @@
 import { useState } from "react"
 import { useNavigate, useParams } from "react-router"
+import styled from "styled-components"
+
+import Container from "../components/layout"
+
+const TaskInput = styled.input`
+    display: block;
+    width: 100%;
+    max-width: 500px;
+    margin-bottom: 10px;
+`
 
 const SingleTodo = () => {
     const {id} = useParams()
     const navigate = useNavigate()
 
+    // TODO: Request task
     const data = {
         id: id,
         task: "Create awesome web app",
         done: true
     }
 
-    const [task, setTask] = useState(data.task)
-    
-    const handleSave = () => {
-        navigate(-1)
+    const [newTask, setNewTask] = useState(data.task)
+
+    const handleSave = (e) => {
+        e.preventDefault()
+
+        if (newTask !== "") {
+            // TODO: Save task
+            navigate(-1)
+        }
+        else {
+            alert("You cannot save an empty task")
+            setNewTask(data.task)
+        }
     }
 
-    const handleCancel = () => {
-        const choice = window.confirm("Discard changes?")
-        if (choice) navigate(-1)
+    const handleCancel = (e) => {
+        e.preventDefault()
+
+        if (newTask !== data.task) {
+            const choice = window.confirm("Discard changes?")
+            if (choice) navigate(-1)
+        } else 
+            navigate(-1)
     }
 
     return (
         <main>
-            <div>
+            <Container>
                 <h1>Editing task "{data.task}"</h1>
-                <input type="text" value={task} onChange={e => setTask(e.target.value)}/>
-                <input type="submit" onClick={() => handleSave()} value="Save"/>
-                <button onClick={() => handleCancel()}> Cancel </button>
-            </div>
+                <form>
+                    <TaskInput 
+                        type="text"
+                        value={newTask}
+                        autoFocus
+                        onChange={e => { setNewTask(e.target.value) }} />
+                    <input type="submit" onClick={(e) => handleSave(e)} value="Save"  />
+                    <button onClick={(e) => handleCancel(e)}> Cancel </button>
+                </form>
+            </Container>
         </main>
     )
 }
